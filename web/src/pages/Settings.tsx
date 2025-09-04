41be6bd8620779e45d6ec6180bae516223b2cf1a
Dindb-dong <dongwook443@yonsei.ac.kr>
2025-09-05 00:28:22 +0900

    build error 4: tsc lint error

--- web/src/pages/Settings.tsx
+++ web/src/pages/Settings.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useQuery } from 'react-query'
 import { Link, Key, Database, TestTube, ExternalLink } from 'lucide-react'
 import { oauthApi, webhookApi, authApi } from '../utils/api'
